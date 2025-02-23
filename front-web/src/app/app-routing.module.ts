ed2446f3b75611945dab357a00520f108a24a2c8
Axell5 <axelcaro034@gmail.com>
2025-02-23 18:12:51 -0500

    inventario-paciente

--- front-web/src/app/app-routing.module.ts
+++ front-web/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InventarioComponent } from './inventario-paciente/inventario-paciente.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'inventario', component: InventarioComponent },
+  { path: '', redirectTo: '/inventario', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front-web/src/app/app.module.ts
+++ front-web/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './componentTools/header/header.component';
+import { InventarioComponent } from './inventario-paciente/inventario-paciente.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    InventarioComponent
   ],
   imports: [
     BrowserModule,
